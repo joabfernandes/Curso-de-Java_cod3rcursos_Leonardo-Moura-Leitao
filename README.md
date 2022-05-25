ab819977200448309f9a2412f906f6860bcee2bb
“joabfernandes” <jjoabfernandes@gmail.com>
2022-05-25 11:45:30 -0300

    Projeto concluído v.01

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java 13 COMPLETO: Do Zero ao Profissional + Projetos Reais!
+Usando o Java SE 16 e o Eclipse IDE 2021‑06
+ 
---
+++ exercícios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,33 @@
+package fundamentos;
+
+/**
+ * Essa classe representa... JAVADOC
+ * 
+ * @author Joab Fernandes <jjoabfernandes@gmail.com>
+ * @since JDK1.0
+ * @See
+ */
+public class PrimeiroPrograma {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Uma sentença de código termina com ;
+		// Mais um comentário...
+		// Fim
+		System.out.println("Primeiro programa Parte #01!");
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3..
+		 */
+		System.out.println("Primeiro programa Parte #02!");
+		
+		System.out.println("Fim"); // Aqui também funciona
+	}
+
+}
